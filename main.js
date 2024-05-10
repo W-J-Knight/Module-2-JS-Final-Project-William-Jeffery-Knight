24b7c3faef03ee27ed2392143cffe93bb073b55f
William Knight <William.Jeffery.Knight@protonmail.com>
2024-05-10 08:36:27 -0400

    Work on ideas on the logic behind the tracking the intventory.

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
-alert('Hello World!!!')
+//intventory class
+// list of store areas
+// add new store area
+// edit store area
+// delete store area
+
+// store area class
+// list of items
+// sort into small lists  examples
+// [{
+//     blackBeans: [
+
+//         { name: 'blackBeans', expirationDate: '12/5' },
+
+//         { name: 'blackBeans', expirationDate: '12/4' },
+
+//         { name: 'blackBeans', expirationDate: '12/5' }]
+// },
+// {
+//     redBeans:
+//         [
+//             { name: 'redBeans', expirationDate: '12/8' },
+
+//             { name: 'redBeans', expirationDate: '12/7' }
+//         ]
+// },
+// {
+//     whiteBeans:
+//         [
+//             { name: 'whiteBeans', expirationDate: '12/5' }
+//         ]
+// },
+
+// {
+//     whiterRice:
+//         [
+//             { name: 'whiterRice', expirationDate: '12/5' }
+//         ]
+// },
+
+
+// {
+//     wildRice:
+//         [
+//             { name: 'wildRice', expirationDate: '12/4' }
+//         ]
+// }
+// ]
+    //sort small list by date
+// add new item
+// edit item
+// delete item
+
+
+// item class
+// add new item
+// move
+// edit item
+// delete item
+
+
+// search for items
