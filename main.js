c1fbf4dae12e62ccb7df248839fcbec45e91d97e
William Knight <William.Jeffery.Knight@protonmail.com>
2024-05-11 20:39:04 -0400

    Work OOP on the inventory. Find way put the items in categories and sort the items by date of expiration.

--- main.js
+++ main.js
@@ -1,58 +1,121 @@
 //intventory class
-// list of store areas
-// add new store area
-// edit store area
-// delete store area
+class Intventory {
+    // static storageAreaList = []
+    constructor() {
+       this.storageAreaList = []
+        
+    }
+    // add new storearea
+    addNewStorageArea(name,type, room="kichen"){
+      this.storageAreaList.push(new StorageArea(name, type,room))
+    }
 
-// store area class
-// list of items
-// sort into small lists  examples
-// [{
-//     blackBeans: [
+    getStorageArea(id){
+        // gett filter array
+        const tempList = this.storageAreaList.filter(function (storageArea){
+            return storageArea.StorageAreaID ==id
+        })
+        // just need the only object in the array
+       return tempList[0]
+    }
 
-//         { name: 'blackBeans', expirationDate: '12/5' },
+    addNewItemToStorageArea(storageAreaID, itemName, itemType, itemExpirationDate, itemAmount, itemLocation){
+        let storageArea = this.getStorageArea(storageAreaID)
+    //   add new to staorage area
+        storageArea.addNewItem(itemName, itemType, itemExpirationDate, itemAmount, storageArea.name)
+    }
 
-//         { name: 'blackBeans', expirationDate: '12/4' },
+    
+}
 
-//         { name: 'blackBeans', expirationDate: '12/5' }]
-// },
-// {
-//     redBeans:
-//         [
-//             { name: 'redBeans', expirationDate: '12/8' },
+// storage area class
+class StorageArea{
+    static id = 0
+    constructor(name, type, room){
+        StorageArea.id++;
+        this.StorageAreaID = StorageArea.id;
+        this.name = name;
+        this.type = type;
+        this.room = room;
+        // list of items
+        this.itemList = []
+    }
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    sortedList(){
+        this.itemList.sort(  (a, b) => {
+            const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+            
+            // names must be equal
+            return 0;
+            })
+          
+    }
+    
+    sortIntoCategories(){            
+        const groupedInt =  this.itemList.reduce((groupedItems, item)=>
+        {
+             const name = item.name
+             if (groupedItems[name] == null) groupedItems[name]= []
+             groupedItems[name].push(item)
+            // https://flaviocopes.com/how-to-sort-array-by-date-javascript/
+            groupedItems[name].sort((a, b) => b.expirationDate - a.expirationDate)
+             return groupedItems
+        },{})
+        
+        return groupedInt
+}
+    
 
-//             { name: 'redBeans', expirationDate: '12/7' }
-//         ]
-// },
-// {
-//     whiteBeans:
-//         [
-//             { name: 'whiteBeans', expirationDate: '12/5' }
-//         ]
-// },
+    
+    // edit storeageArea
+    editStorageArea(){
+    
+    }
+    
+    addNewItem(name, type, date, amount ,loction){
+        this.itemList.push(new Item(name,type, date, amount ,loction))
+    }
 
-// {
-//     whiterRice:
-//         [
-//             { name: 'whiterRice', expirationDate: '12/5' }
-//         ]
-// },
+    removeItem(id){
+        this.itemList = this.itemList.filter((item)=>{
+            return item.itemID != id
+        })
+    }
+    
 
+    removeToDifferentStoreage(storageAreaID){
 
-// {
-//     wildRice:
-//         [
-//             { name: 'wildRice', expirationDate: '12/4' }
-//         ]
-// }
-// ]
-    //sort small list by date
-// add new item
-// edit item
-// delete item
+    }
+}
 
+class Item{
+    static id = 0;
+    constructor(name, type, expirationDate, amount,location){
+        Item.id++;
+        this.itemID = Item.id;
+        this.name = name;
+        this.type = type;
+        this.expirationDate = new Date(expirationDate);
+        this.amount = amount;
+        this.location = location;
+    }
+    moveItem(){
 
-// item class
+    }
+    editItem(){
+
+    }
+    deleteItem(){
+
+    }
+}
 // add new item
 // move
 // edit item
@@ -60,3 +123,24 @@
 
 
 // search for items
+
+// testing------------
+const intventory1 = new Intventory()
+intventory1.addNewStorageArea('wallCabinet-1',"roomTemp")
+intventory1.addNewStorageArea('baseCabinet-1',"roomTemp")
+intventory1.addNewStorageArea('wallCabinet-2',"roomTemp")
+intventory1.addNewStorageArea('baseCabinet-2',"roomTemp")
+
+console.log(intventory1.storageAreaList)
+console.log("get-1",intventory1.getStorageArea(2))
+intventory1.addNewItemToStorageArea(2, "whiterRice", "roomTemp", "2024-2-6", "14oz")
+intventory1.addNewItemToStorageArea(2, "whiterRice", "roomTemp", "2024-3-6", "14oz")
+intventory1.addNewItemToStorageArea(2, "whiterRice", "roomTemp", "2024-1-6", "14oz")
+intventory1.addNewItemToStorageArea(2, "beans", "roomTemp", "2024-12-6", "14oz")
+intventory1.addNewItemToStorageArea(2, "beans", "roomTemp", "2024-5-6", "14oz")
+intventory1.addNewItemToStorageArea(2, "beans", "roomTemp", "2024-6-6", "14oz")
+
+ intventory1.getStorageArea(2).sortedList()
+ const x = intventory1.getStorageArea(2).sortIntoCategories()
+console.log("get-2", intventory1.getStorageArea(2).itemList)
+console.log('x',x)
